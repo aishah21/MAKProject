dc42ba67ed0e84d777221ebe50bffdab912eae65
aishah <aishah-almaghrabi@hotmail.com>
2018-05-11 04:57:19 +0300

    first edtion

---
+++ page/home/header.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+    <meta name="viewpower" content="width=device-width, initial-scale=1"><!-- thisi is very importent for responsive website -->
+
+	<!--Google fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"> 
+	
+     <link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../../css/css.css">
+	
+
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.min.css">
+	<!-- Font Awesom -->
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    
+
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-flipped.css">
+  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
---
+++ page/home/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php require "header.php";?>
+</head>
+<body class="boy">
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 pan">
+				<div class="panel panel-default">
+					<div class="panel-heading ">
+						<div class="text-center">
+							تسجيل دخول 
+						</div>
+					</div>
+					<div class="panel-body">
+						<form >
+							<input type="email" name="email" class="form-control log" placeholder="البريد الالكتروني ">
+							<input type="password" name="pass" class="form-control log" placeholder="الرقم السري ">
+							<input type="submit" name="subm" class="btn btn-default log but" value="تسجيل دخول ">
+						</form>
+					</div>
+					<div class="panel-footer">
+						<a href="#">تواصل مع الدعم افني </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<script type="text/javascript" src="../../js/jquery.js"></script>
+  <script type="text/javascript" src="../../js/bootstrap.js"></script>
+  <script type="text/javascript" src="../../js/custom.js"></script>
+</body>
+</html>
